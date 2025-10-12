2ebbc8aec93362568c914cdf0f89223a9c3ed5c1
Equacare <equacare@Mac.lan>
2025-10-12 13:37:44 -0700

    Fix S3 ACL error - disable ACLs for modern S3 buckets

--- equacare_project/settings.py
+++ equacare_project/settings.py
@@ -151,10 +151,13 @@ if USE_S3:
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'us-east-1')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    AWS_DEFAULT_ACL = 'public-read'
+    AWS_DEFAULT_ACL = None
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
     }
+    # Disable ACLs for modern S3 buckets
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_S3_VERIFY = True
     
     # Static files
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
